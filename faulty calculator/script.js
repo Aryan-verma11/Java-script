7dbb0019e3ea73ad3f32c9e3a5b1348ac0f9c2e5
Aryan Verma <av868400@gmail.com>
2025-10-26 14:39:07 +0530

    strings

---
+++ faulty calculator/script.js
@@ -0,0 +1,9 @@
+function calc(a,b) {
+    return a+b
+}
+
+
+
+
+sum1=calc(3,5)
+console.log(sum1)
---
+++ strings/strings.js
@@ -0,0 +1,26 @@
+console.log("hey we are going to learn about strings")
+let a ="aryan"
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5])  this will give undefined bcz thre is no more indexing is available
+
+console.log(a.length)
+let my_name="aryan"
+let friend="kammo"
+console.log("his friend name is " + friend + " and his name is "+ my_name +" ")
+
+console.log(`his name is ${my_name} his friend name is ${friend}`)  //known as template literals `known as backtick`
+
+
+let b="aryan"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,3))
+console.log(b.slice(1))
+console.log(a.replace("yan","nu"))
+console.log(b.concat(a,"raj","rahul","ram"))
+console.log(a)
