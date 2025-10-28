5159c3f53e51fa753474d43d0532e35bf71c7c34
Aryan Verma <av868400@gmail.com>
2025-10-28 22:32:10 +0530

    name generator

---
+++ business name generator/script.js
@@ -0,0 +1,17 @@
+console.log("making business name generator by combining list of adjectives ")
+
+// adjective list
+// crazy
+// Amazing
+// fire
+
+// shop name
+// Engine
+// foods
+// garments
+
+// another words:
+// bros 
+// limited 
+// hub
+
