8ce03c0c9ca21a182629ddcb462c57e630100ab3
Aryan Verma <av868400@gmail.com>
2025-10-31 22:41:10 +0530

    fixed array

--- factorial/script.js
+++ factorial/script.js
@@ -5,8 +5,16 @@
 
 
 let a=6;
+let arr=[]
 
-for (i=1; i<a; i++ ) {
-    console.log(i);
+for (i=a; i>=1; i--) {
+    return arr
+}
+console.log(arr);
+
+let arr2=[1,2,3,4,5,6,7,8,9]
+const reducefunckanaam=(a,b)=>{
+    return a+b  //koi bhi operation laag skty hai
 }
 
+console.log(arr2.reduce(reducefunckanaam))
