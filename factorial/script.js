5aeef82094f31d1d7d3bdfcd34cf27fae4e2d0f7
Aryan Verma <av868400@gmail.com>
2025-11-03 22:22:47 +0530

    factorial added

--- factorial/script.js
+++ factorial/script.js
@@ -1,30 +1,12 @@
-// write a program to calculate the factorial of a number by using reduce and for loop
+// Factorial using filter and reduce
 
-//6!=5x4x3x2x1
+function factorial(n) {
+  if (n < 0) return "Factorial not defined for negative numbers";
 
-
-
-let a=6;
-let arr=[]
-
-for (i=a; i>=1; i--) {
-    return arr
-}
-console.log(arr);
-
-let arr2=[1,2,3,4,5,6,7,8,9]
-const reducefunckanaam=(a,b)=>{   
-    return a+b  //koi bhi operation laag skty hai
+  return Array.from({ length: n + 1 }, (_, i) => i) // [0, 1, 2, ..., n]
+    .filter(num => num > 0)                        // remove 0
+    .reduce((acc, curr) => acc * curr, 1);         // multiply all numbers
 }
 
-console.log(arr2.reduce(reducefunckanaam))
-
-//using filter method agar hmmey kuch specific elements chahiye tab use krty hai jaisy ki wo elements jo ki 5 sy badey hai\
-
-const greaterthanfive=(e)=>{
-    if(e>5){
-        return true
-    }
-    return false
-}
-console.log(arr.filter(greaterthanfive));
+// Example:
+console.log(factorial(5)); // Output: 120
