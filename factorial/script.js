9151aaf1ddf86b4e44341459cf5d369ab4901dac
Aryan Verma <av868400@gmail.com>
2025-11-04 22:34:53 +0530

    commit

--- factorial/script.js
+++ factorial/script.js
@@ -10,3 +10,39 @@ function factorial(n) {
 
 // Example:
 console.log(factorial(5)); // Output: 120
+// Factorial using filter and reduce
+
+function factorial(n) {
+  if (n < 0) return "Factorial not defined for negative numbers";
+
+  return Array.from({ length: n + 1 }, (_, i) => i) // [0, 1, 2, ..., n]
+    .filter(num => num > 0)                        // remove 0
+    .reduce((acc, curr) => acc * curr, 1);         // multiply all numbers
+}
+
+// Example:
+console.log(factorial(5)); // Output: 120
+// Factorial using filter and reduce
+
+function factorial(n) {
+  if (n < 0) return "Factorial not defined for negative numbers";
+
+  return Array.from({ length: n + 1 }, (_, i) => i) // [0, 1, 2, ..., n]
+    .filter(num => num > 0)                        // remove 0
+    .reduce((acc, curr) => acc * curr, 1);         // multiply all numbers
+}
+
+// Example:
+console.log(factorial(5)); // Output: 120
+// Factorial using filter and reduce
+
+function factorial(n) {
+  if (n < 0) return "Factorial not defined for negative numbers";
+
+  return Array.from({ length: n + 1 }, (_, i) => i) // [0, 1, 2, ..., n]
+    .filter(num => num > 0)                        // remove 0
+    .reduce((acc, curr) => acc * curr, 1);         // multiply all numbers
+}
+
+// Example:
+console.log(factorial(5)); // Output: 120
