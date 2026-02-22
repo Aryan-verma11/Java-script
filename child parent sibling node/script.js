44c1a41c7b99d883abd7ae04f78027d0635ff39b
Aryan Verma <av868400@gmail.com>
2026-02-22 22:56:28 +0530

    commit

---
+++ child parent sibling node/script.js
@@ -0,0 +1,13 @@
+console.log("hello word");
+document.body.firstElementChild
+document.body.lastElementChild.childNodes
+document.body.firstElementChild.children[0]
+document.body.firstElementChild.children[1]
+document.body.firstElementChild.children[2]
+document.body.firstElementChild.children[3]
+document.body.firstElementChild.children[4]
+document.body.firstElementChild.children[4].previousElementSibling
+document.body.firstElementChild.children[4].nextElementSibling
+document.body.firstElementChild.children[4].nextSibling 
+// only gives text node which is empty text node
+                                                   
