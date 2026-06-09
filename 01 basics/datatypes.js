509d4ee75c4eaa7e2c0c379ced461debeeaac2ad
Aryan Verma <av868400@gmail.com>
2026-06-09 13:36:14 +0530

    js basic

---
+++ 01 basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"// treat all js code as new and modern
+// alert() works in browser in node js
+
+
+let name = "aryan verma"
+let age = 22
+let isLoggedIn = false
+
+
+// Types of data types
+//these are primitive data types
+/*
+number=>2 to the power 53
+big int
+string ""
+boolean t/f
+null => standalone value value abhi assign nhi hua hai
+undefined => value is not assigned yet
+symbol => unique identifier
+
+
+*/
+
+console.log(typeof null) //here null is object but it is not an object it is a primitive data type
