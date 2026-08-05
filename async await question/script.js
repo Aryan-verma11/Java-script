85392169147f3a639cf72ee4e0d9aa009e9857a1
Aryan Verma <av868400@gmail.com>
2026-08-05 13:51:14 +0530

    async await question completed

--- async await question/script.js
+++ async await question/script.js
@@ -1,3 +1,40 @@
-let div = document.createElement('div');
-div.textContent ="initializing hacking";
-box.appendChild(div);
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+const messages = [
+  "Initializing Hacking",
+  "Reading your Files",
+  "Password files Detected",
+  "Connecting to Server",
+  "Uploading Personal Data",
+  "Deleting System Logs",
+  "Hack Successful",
+];
+
+async function showMessages(msg) {
+  let div = document.createElement("div");
+  box.appendChild(div);
+
+  div.textContent = msg;
+
+  for (let i = 1; i <= 5; i++) {
+    await delay(1000);
+
+    div.textContent = msg + ".".repeat(i);
+  }
+
+  await delay(1000);
+  div.textContent = msg;
+}
+
+async function main() {
+  for (let msg of messages) {
+    await showMessages(msg);
+    await delay(Math.random() * 2000 + 1000);
+  }
+}
+
+main();
